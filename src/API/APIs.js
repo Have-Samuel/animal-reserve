0007109adef5aeba47525253f7889e2d9772e66c
Have-Samuel <samiehave9@gmail.com>
2023-04-03 04:34:35 +0300

    Changes
    Changes

--- src/API/APIs.js
+++ src/API/APIs.js
@@ -1,5 +1,5 @@
 export default async function fetchAnimalData() {
-  const baseURL = 'https://brianiswu-cat-facts-v1.p.rapidapi.com/facts';
+  const baseURL = 'https://zoo-animal-api.herokuapp.com/animals/rand/10';
   const response = await fetch(baseURL);
   if (response.status !== 200) {
     throw new Error('Please check your endpoint');
