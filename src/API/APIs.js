6605f6b25c59fa4c8d5113891b21038cc540e7a3
Have-Samuel <samiehave9@gmail.com>
2023-04-03 04:33:19 +0300

    Changing the Api
    Changing the Api

--- src/API/APIs.js
+++ src/API/APIs.js
@@ -1,5 +1,5 @@
 export default async function fetchAnimalData() {
-  const baseURL = 'https://zoo-animal-api.herokuapp.com/animals/rand/10';
+  const baseURL = 'https://brianiswu-cat-facts-v1.p.rapidapi.com/facts';
   const response = await fetch(baseURL);
   if (response.status !== 200) {
     throw new Error('Please check your endpoint');
